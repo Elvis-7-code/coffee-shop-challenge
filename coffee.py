c3f2609108e9a508e34170813b822f974aa60f91
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-20 01:23:24 +0300

    order-coffee.py

--- coffee.py
+++ coffee.py
@@ -1,7 +1,8 @@
+from order import Order
 class Coffee:
     def __init__(self, name):
         if not isinstance(name, str):
-            raise ValueError("Name must be a string")
+            raise ValueError("Coffee name must be a string")
         if len(name) < 3:
             raise ValueError("Name must be at least 3 characters long")
         self._name = name
@@ -10,4 +11,8 @@ class Coffee:
         return self._name
     @name.setter
     def name(self, value):
-        raise AttributeError("Cannot modify name after initialization.")    
+        raise AttributeError("Cannot modify name after initialization.") 
+       
+    def orders(self):
+        return[order for order in Order.all_orders if order.coffee == self] 
+      
