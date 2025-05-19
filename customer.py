94fc22209a12a7aaa69dd0c9ab088f54bcb8cb88
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-19 21:56:37 +0300

    creat_order-cust.py

--- customer.py
+++ customer.py
@@ -1,6 +1,8 @@
+from order import Order
 class Customer:
     def __init__(self, name):
         self.name = name
+        Customer.all_customers.append(self)
     @property
     def name(self):
         return self._name
@@ -9,6 +11,14 @@ class Customer:
     def name(self, value):
         if not isinstance(value, str):
             raise ValueError("Name must be a string")
-        if not (1 <= len(value) <= 50):
-            raise ValueError("Name must be between 1 and 50 characters long")
+        if not (1 <= len(value) <= 15):
+            raise ValueError("Name must be between 1 and 15 characters long")
         self._name = value
+
+    def create_order(self, coffee, price):
+        return order(self, coffee, price)
+    
+    
+    
+
+            
