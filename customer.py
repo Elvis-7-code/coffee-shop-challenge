4e544579190c631edae66d60e85b329d37fdcf9f
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-20 00:52:13 +0300

    most_aficionado-cust.py

--- customer.py
+++ customer.py
@@ -23,6 +23,18 @@ class Customer:
     
     def coffees(self):
         return list (set(order.coffee for order in self.orders()))
+    
+    @classmethod
+    def most_aficionado(cls, coffee):
+        # Customer who spent the most on a specific coffee
+        max_spent = 0
+        top_customer = None
+        for customer in cls.all_customers:
+            total_spent = sum(order.price for order in customer.orders()if order.coffee == coffee)
+            if total_spent > max_spent:
+                max_spent = total_spent
+                top_customer = customer
+            return top_customer
 
     
 
