51bbfd6f210b130213d90ab9ee30342d6bdff8f6
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-20 01:11:00 +0300

    test_valid_order_creation

--- tests/order_test.py
+++ tests/order_test.py
@@ -7,3 +7,13 @@ from coffee import Coffee
 def reset_orders():
     Order.all_order.clear
     # Automatically clears all order before each test
+
+def test_valid_order_creation():
+    customer = Customer("Elviss")
+    coffee = Coffee("Latte")
+    order = Order(customer, coffee, 4.5)
+
+    assert order.customer == customer  
+    assert order.coffee == coffee
+    assert order.price == 4.5
+    assert order in Order.all_orders  
