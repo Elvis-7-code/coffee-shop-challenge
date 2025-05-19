e0204b20cf75190e9bda229bd367513775177798
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-20 01:35:46 +0300

    latte, mocha-debug

--- debug.py
+++ debug.py
@@ -1,3 +1,10 @@
 from customer import Customer
 from coffee import Coffee
 from order import Order
+
+c1 = Customer("Elviss")
+c2 = Customer("Tina")
+
+latte = Coffee("Latte")
+mocha = Coffee("Mocha")
+
