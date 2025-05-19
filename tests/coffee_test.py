b1e65e81438e7af128b7b1fc0fa86f2a67451158
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-20 01:30:33 +0300

    coffee_initilization-coff.test.py

--- tests/coffee_test.py
+++ tests/coffee_test.py
@@ -2,3 +2,9 @@ import pytest
 from coffee import Coffee
 from customer import Customer
 from order import Order
+
+def test_coffee_initialization_and_name():
+    coffee = Coffee("Espresso")
+    assert coffee.name == "Espresso"
+
+def    
