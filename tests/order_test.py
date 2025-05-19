9566aab3c1f856391019da462fa2472f8d169d06
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-20 01:07:59 +0300

    reset_orders-ord.test

--- tests/order_test.py
+++ tests/order_test.py
@@ -2,3 +2,8 @@ import pytest
 from order import Order
 from customer import Customer
 from coffee import Coffee
+
+@pytest.fixture(autouse=True)
+def reset_orders():
+    Order.all_order.clear
+    # Automatically clears all order before each test
