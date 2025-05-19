0598afa636769e3706030da7ae7c61d79cda7c89
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-20 01:27:34 +0300

    average_price-coffee.py

--- coffee.py
+++ coffee.py
@@ -21,3 +21,10 @@ class Coffee:
     
     def num_orders(self):
         return len(self.order())
+    
+    def average_price(self):
+        orders = self.orders()
+        if not orders:
+            return 0
+        total = sum(order.price for order in orders)
+        return total / len(orders)
